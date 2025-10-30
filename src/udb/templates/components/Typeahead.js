65a865cededcbe73f4d587ead31d0617521f20d3
Patrik Dufresne <patrik@ikus-soft.com>
2025-10-30 11:07:52 -0400

    Update Typeahead component
    Signed-off-by: Patrik Dufresne <patrik@ikus-soft.com>

--- src/udb/templates/components/Typeahead.js
+++ src/udb/templates/components/Typeahead.js
@@ -1,9 +1,8 @@
-/**
- * Typeahead configured using data-* attributes
- */
-jQuery(function () {
-    $('.js-typeahead').each(function (_idx) {
+/* Initialize Typeahead */
+jQuery(function() {
+    $('.js-typeahead').each(function(_idx) {
         const cfg = $(this).data();
-        $(this).typeahead(cfg);
+        const typeahead = $(this).typeahead(cfg);
+        $(this).data('typeahead', typeahead);
     });
 });
