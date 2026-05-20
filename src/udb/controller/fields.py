476393936e6ef174bdd4d59f7da4fc9200bbb4a6
Patrik Dufresne <patrik@ikus-soft.com>
2026-05-20 10:46:49 -0400

    doc: update copyright year to 2026 across codebase
    Signed-off-by: Patrik Dufresne <patrik@ikus-soft.com>

--- src/udb/controller/fields.py
+++ src/udb/controller/fields.py
@@ -1,5 +1,5 @@
 # udb, A web interface to manage IT network
-# Copyright (C) 2025 IKUS Software inc.
+# Copyright (C) 2026 IKUS Software inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
